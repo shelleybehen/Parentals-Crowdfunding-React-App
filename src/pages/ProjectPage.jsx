f54003300dd8e4822a048086ee986cd7767191a9
shelleybehen <shelleybehen@gmail.com>
2021-11-03 19:54:30 +1000

    Update ProjectPage.jsx

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,35 +2,158 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 
-function ProjectPage() {
-    const [projectData, setProjectData] = useState({pledges: [] });
-    const { id } = useParams();
+const ProjectPage = () => {
+    const [projectData, setProjectData] = useState({ pledges: [] });
+    const [isEditing, setIsEditing] = useState(false);
+    const { id: project_id } = useParams();
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
-        .then((results) => {
-        return results.json();
-        })
-        .then((data) => {
-        setProjectData(data);
-        });
-    }, [id]);
+        fetch(`${process.env.REACT_APP_API_URL}projects/${project_id}`)
+          .then((results) => {
+            return results.json();
+          })
+          .then((data) => {
+            setProjectData(data);
+          });
+      }, [ project_id ]);
 
+//     return (
+//     <div>
+//         <h2>{projectData.title}</h2>
+//         <h3>Created at: {projectData.date_created}</h3>
+//         <h3>{`Status: ${projectData.is_open}`}</h3>
+//         <h3>Pledges:</h3>
+//         <ul>{projectData.pledges.map((pledgeData, key) =>{
+//             return (
+//             <li>{pledgeData.amount} from {pledgeData.supporter}
+//             </li>
+//             );
+//         })}
+//         </ul>
+//         </div>
+//         );
+// }
+
+const handleChange = (event) => {
+    const { id, value } = event.target
+    console.log("We are updating the ", id, " to be: ", value)
+    setProjectData({
+      ...projectData,
+      [id]: value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await fetch(`
+      ${process.env.REACT_APP_API_URL}projects/${project_id}`, {
+      method: "put",
+      headers: {
+        "Authorization": `Token ${localStorage.getItem('token')}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 
+        title: projectData.title,
+        description: projectData.description,
+        amount: projectData.amount,
+        image: projectData.image,
+        is_open: projectData.is_open
+      }),
+    })
+  }
+
+  const ReadProject = () => {
     return (
-    <div>
-        <h2>{projectData.title}</h2>
-        <h3>Created at: {projectData.date_created}</h3>
-        <h3>{`Status: ${projectData.is_open}`}</h3>
+      <div>
+        <h1>{projectData.title}</h1>
+        <h2>{projectData.description}</h2>
+        <h3>Created at: {new Date(projectData.date_created).toDateString()}</h3>
+        <h3>{`Is Open to pledges: ${projectData.is_open}`}</h3>
         <h3>Pledges:</h3>
-        <ul>{projectData.pledges.map((pledgeData, key) =>{
+        <ul>
+          {projectData.pledges.map((pledgeData, key) => {
             return (
-            <li>{pledgeData.amount} from {pledgeData.supporter}
-            </li>
-            );
-        })}
+              <li key={key}>
+                {pledgeData.amount} from {pledgeData.supporter}
+              </li>
+            )
+          })}
         </ul>
-        </div>
-        );
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      { localStorage.getItem('token') 
+        && isEditing === false 
+        && <button onClick={() => setIsEditing(true)}>Edit Project</button> 
+      }
+      <div>
+      {
+        isEditing
+        ? (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="title">Title:</label>
+              <input
+                value={projectData.title}
+                type="text"
+                id="title"
+                placeholder=""
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="description">Description:</label>
+              <input
+                value={projectData.description}
+                type="text"
+                id="description"
+                placeholder=""
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="goal">Goal:</label>
+              <input
+                value={projectData.amount}
+                type="text"
+                id="amount"
+                placeholder=""
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="image">Image:</label>
+              <input
+                value={projectData.image}
+                type="text"
+                id="image"
+                placeholder=""
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="is_open">Open:</label>
+              <input
+                value={projectData.is_open}
+                type="text"
+                id="is_open"
+                placeholder=""
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">Submit</button>
+            <button onClick={() => setIsEditing(false)}>Cancel</button> 
+          </form>
+        )
+        : <ReadProject />
+      }
+      </div>
+    </div>
+  )
 }
+
 
 export default ProjectPage;
