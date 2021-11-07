4c53b7d2eeb29e8a07a740f0de5e5787b062aa8e
shelleybehen <shelleybehen@gmail.com>
2021-11-07 16:35:16 +1000

    Added pledge css

--- src/components/CreatePledgeForm/CreatePledgeForm.jsx
+++ src/components/CreatePledgeForm/CreatePledgeForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from "react-router";
-import './CreateProjectForm.css'
+import './CreatePledgeForm.css'
 // test
 
 const CreatePledgeForm = () => {
